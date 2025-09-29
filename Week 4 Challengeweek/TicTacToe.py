892a8ce4e2af157326ba2d74cd1a40dcb28d4e02
Christopher Pereira <christopher.pereiraflores@student.odisee.be>
2025-09-29 18:44:06 +0200

    eerste versie game logic + aanpassingen valid checker

--- Week 4 Challengeweek/TicTacToe.py
+++ Week 4 Challengeweek/TicTacToe.py
@@ -34,26 +34,97 @@ def winner_checker_tic_tac_toe(board_dictionary :dict):
         value3 = board_dictionary[list[2]]
         if value1 == value2 and value2 == value3:
             if value1 == "X":
-                return "Player 1 wins!"
+                print("Player 1 wins")
+                return 1
             elif value2 == "O":
-                return "Player 2 wins!"
+                print("Player 2 wins")
+                return 2
             else:
                 continue
-    return "No winner yet!"            
+    return False            
+
+ 
+ 
+def is_count_valid():
+    input_valid = False
+    while input_valid == False:
+        player_count = input("Hoeveel spelers zijn er?")
+        if player_count == "1" or player_count == "2":
+            input_valid = True
+            return player_count
+            break
+        else:
+            print("input error")
+ 
+def is_extra_game():
+    extra_game_input = input("One more game?")
+    extra_game_input_lower = extra_game_input.lower()
+    if extra_game_input_lower == "no":
+        extra_game = False
+        return False
+    return True
+    
+def single_player_tic_tac_toe():
+    print("WIP")
+
+
+def two_player_tic_tac_toe(game_count :int, player_list :list[str]):
+    board = {
+    "A1":" ",
+    "A2":" ",
+    "A3":" ",
+    "B1":" ",
+    "B2":" ",
+    "B3":" ",
+    "C1":" ",
+    "C2":" ",
+    "C3":" ",}
+    turn = 1
+    game_is_over = False
+    while not game_is_over:
+        player_turn = turn % 2
+        player_name = player_list[player_turn]
+        input_player = input(f"{player_name} please input your move: ")
+        while not game_input_checker(input_player, board):
+            input_player = input(f"{player_name} please input a valid move: ")
+        if player_turn == 1:
+            board[input_player] = "X"
+        else:
+            board[input_player] = "O"
+        turn += 1
+        boardgame_printer(board)
+        game_is_over = winner_checker_tic_tac_toe(board)
+    if game_is_over == 1:
+        print(player_list[1], "has won!")
+        winner = player_list [1]
+        loser = player_list[0]
+    else :
+        print(player_list[0], "has won!")
+        winner = player_list [0]
+        loser = player_list[1]
+        player_list = [loser,winner]
+    return player_list
+
+
+
 
         
       
 def main ():
-    board = {
-    "A1":"X",
-    "A2":"O",
-    "A3":"X",
-    "B1":" ",
-    "B2":"X",
-    "B3":" ",
-    "C1":"X",
-    "C2":"O",
-    "C3":" ",}
-    print(winner_checker_tic_tac_toe(board))
+    game_count = 1
+    extra_game = True
+    player_count = int(is_count_valid())
+    player_list= ["Christopher","Dimitri"]
+    while extra_game:
+        if player_count == 1:
+            print("Het spel zal beginnen met 1 Speler")
+            single_player_tic_tac_toe()
+        else:
+            print("Het spel zal beginnen met 2 Spelers")
+            player_list = two_player_tic_tac_toe(game_count,player_list)
+    extra_game = is_extra_game()
+    if extra_game:
+        game_count += 1
+
 if __name__ == "__main__":
-    main()    
+    main()
