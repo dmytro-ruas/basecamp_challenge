e79d18ce0ab8a1cba6023e6a9d8fb1002a1da573
dmytro-ruas <dmytro.nazarenko2007@gmail.com>
2025-12-08 10:44:57 +0100

    Update hand.py

--- week_12_challenge/hand.py
+++ week_12_challenge/hand.py
@@ -1,5 +1,13 @@
-import card
 class Hand:
     def __init__(self) -> None:
         self.cards: list[card.Card] = []
+        pass
+
+    def show_hand(self):
+        pass
+
+    def remove_card(self, card_name):
+        pass
+
+    def add_card(self):
         pass
