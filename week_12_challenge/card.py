590a05b3bc717634bfd6cc37fe23850eb55935f2
Christopher <1119528@hr.nl>
2025-12-08 10:39:12 +0100

    Created first version of hand.py and json file

--- week_12_challenge/card.py
+++ week_12_challenge/card.py
@@ -1,4 +1,54 @@
 class Card:
-    def __init__(self) -> None:
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def play_card(self):
+            match self.value:
+                case "Skip":
+                    self.skip()
+                case "Attack":
+                      self.attack()
+                case "Defuse":
+                      self.defuse()
+                case "Predict_Future":
+                      self.predict_future()
+                case "Shuffle":
+                      self.shuffle()
+                case "Nope":
+                      self.nope(),
+                case "Favor":
+                      self.favor()
+                case "Bomb":
+                      self.bomb()
+                
+
+    def skip(self):
+         pass
+    
+    def attack(self):
+         pass
+    
+    def defuse(self):
+         pass
+    
+    def predict_future(self):
+         pass
+    
+    def shuffle(self):
+         pass
+    
+    def nope(self):
         pass
+
+    def favor(self):
+        pass
+
+    def bomb(self):
+         pass
+    
+    
+
+                     
+
+
     
