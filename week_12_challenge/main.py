66e3a0d681ecdd3a528976c29c704cfbc1e61376
Christopher <1119528@hr.nl>
2025-12-10 09:58:49 +0100

    Attempt 2

--- week_12_challenge/main.py
+++ week_12_challenge/main.py
@@ -56,21 +56,7 @@ def start_game():
 def tutorial_view():
     pass
 
-def test():
-    deck = create_deck(2)
-    Test_Hand = Hand()
-    Test_Hand.show_hand()
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    deck = Test_Hand.add_card(deck)
-    Test_Hand.show_hand()
-
+    
 if __name__ == "__main__":
     test()
     #start_game()
